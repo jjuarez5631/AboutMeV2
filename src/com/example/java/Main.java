718f2841c8eacefc6903466c6e47812f162bf975
Jose Juarez <jjuarez5631@students.mchenry.edu>
2017-09-04 22:49:03 -0500

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,13 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello There!");
+	String userName = "Jose Juarez";
+	System.out.println("My name is " + userName);
+	System.out.println("I am currently talking Android App development at MCC");
+    System.out.println("I have always had a huge interest in Apps and how they were created");
+    System.out.println("Hope this will give me a good insight!");
+    }
+}
